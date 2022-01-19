3ff4434481fad12dc18cd0f770d8f1733dd4b3af
Pargeo <pargeo82@gmail.com>
2022-01-19 18:51:48 +0100

    move all logic to calendar file

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,4 +1,8 @@
-import { mjeseci, dani, d, year, mjesec } from "./mjeseci.js"
+// import { dani, mjesec } from "./mjeseci.js"
+
+const d = new Date();
+let year = d.getFullYear();
+console.log(year);
 
 function calcdan() {
     let dan = d.getDay();
@@ -8,6 +12,75 @@ function calcdan() {
         return dani[dan - 1];
     }
 };
+
+// testing another year
+// d.setFullYear(2024);
+// year = d.getFullYear();
+console.log(year);
+
+function leapYear() {
+    if ((year - 2020) % 4 === 0) {
+        return 29;
+    } else return 28;
+};
+
+const mjeseci = [{
+        name: "Siječanj",
+        days: 31
+    },
+    {
+        name: "Veljača",
+        days: leapYear()
+    },
+    {
+        name: "Ožujak",
+        days: 31
+    },
+    {
+        name: "Travanj",
+        days: 30
+    },
+    {
+        name: "Svibanj",
+        days: 31
+    },
+    {
+        name: "Lipanj",
+        days: 30
+    },
+    {
+        name: "Srpanj",
+        days: 31
+    },
+    {
+        name: "Kolovoz",
+        days: 31
+    },
+    {
+        name: "Rujan",
+        days: 30
+    },
+    {
+        name: "Listopad",
+        days: 31
+    },
+    {
+        name: "Studeni",
+        days: 30
+    },
+    {
+        name: "Prosinac",
+        days: 31
+    },
+];
+
+const dani = ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja'];
+
+let mjesec = mjeseci[d.getMonth()];
+
+// testing another month
+// d.setMonth(1);
+// mjesec = mjeseci[d.getMonth()]
 
 const parentTable = document.querySelector('tbody');
 const table = () => {
